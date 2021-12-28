7d8d3cc1bb8b4e6ab6893f86b4b8459bb7f3a425
Abdirahman <rizetracker@gmail.com>
2021-12-28 19:12:26 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import FavCategorieen from "./components/FavCategorieen";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import Nieuws from "./components/Nieuws";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Nav />
-      <main class="flex-grow">
-        <FavCategorieen />
-        <Nieuws />
-      </main>
+          <main className="flex-grow">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+          </main>
       <Footer />
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FavCategorieen from "../components/FavCategorieen";
+import Nieuws from "../components/Nieuws";
+
+const Home = () => {  
+    return (
+        <>
+            <FavCategorieen />
+            <Nieuws />
+        </>
+    );
+}
+
+export default Home;
